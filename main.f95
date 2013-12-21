e2e719d17da3645887f45a44d1c70fdf103ff9cb
m87 <m87blackhole@gmail.com>
2013-12-21 15:38:06 +0000

    init neuron

--- main.f95
+++ main.f95
@@ -1,6 +1,9 @@
 program main
     use class_Neuron
     implicit none   
+    
+    type(Neuron) :: neu
+    call init_Neuron(neu,0,0,0,2)
 
 stop
 
--- neuron.f95
+++ neuron.f95
@@ -1,19 +1,33 @@
 module class_Neuron
     use weights
     !use vector
-    implicit none
-    private     
+implicit none
+!    private :: init_Neuron     
     public :: Neuron!, feedForward, setOutput, getOutput, calcOutputGradients, calcHiddenGradients, adaptWeights, getWeights
-    
+     
     type Neuron
-       integer :: output_num
        integer :: id
-       real :: eta
+       integer :: eta
        integer :: alpha
        integer :: no
-       type(tweights), dimension(:),allocatable :: outputWeights 
+       
+       type(tweights), dimension(:), allocatable :: outputWeights
     end type Neuron
-!contains
-!    subroutine
+contains
+    subroutine init_Neuron(this,id,eta,alpha,no)
+        type(Neuron) :: this
+        integer :: i, id,eta,alpha,no
+        this%id = id
+        this%eta = eta
+        this%alpha = alpha
+        this%no = no
+ 
+        allocate(this%outputWeights(0:this%no)) 
+        init_loop: do, i=0, this%no
+           this%outputWeights%weight = 1.0 !rand here 
+        end do init_loop
+         
+        write(*,*) this%outputWeights%weight 
 
+    end subroutine init_Neuron
 end module class_Neuron      
