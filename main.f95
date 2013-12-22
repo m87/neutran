031126985ecfe0a2462b254cb9aaeec07c985401
m87 <m87blackhole@gmail.com>
2013-12-22 16:52:00 +0000

    -

--- main.f95
+++ main.f95
@@ -4,12 +4,28 @@ program main
     
 
     integer, dimension(0:2) :: topology
+    real, dimension(0:2) :: input
+    real, dimension(0:0) :: targets
     integer :: i
     type(Net) :: net1
-    topology(0)=10
-    topology(1)=5
+    topology(0)=3
+    topology(1)=2
     topology(2)=1
-    call init_net(net1,topology,3,0.0,0.0)
+
+    input(0)=1.0
+    input(1)=0.0
+    input(2)=1.0
+
+    targets(0)=1.0
+
+    call init_net(net1,topology,3,0.05,0.1)
+    write(*,*) net1%layers(1)%neurons(0)%outputWeights%weight
+    do, i=0, 10
+        call feedForwardNet(net1, input,3)
+        call backProp(net1,targets,1 )
+   ! write(*,*) net1%layers(2)%neurons%output
+    write(*,*) net1%layers(1)%neurons(0)%outputWeights%weight
+    end do
 
 stop
 
--- net.f95
+++ net.f95
@@ -13,3 +13,61 @@ subroutine init_net(this, topology, n, eta, alpha)
         end do init_loop
  
 end subroutine init_net
+
+
+subroutine backProp(this, targets,n)
+    use types
+    type(Net) :: this
+    type(Layer) :: last
+    real, dimension(0:n-1) :: targets
+    integer :: i,ln 
+    real :: delta
+
+    last = this%layers(this%lnum)
+    this%error = 0.0
+
+    do, i=0, last%n
+        delta = targets(i) - last%neurons(i)%output
+        error = error + delta*delta
+    end do
+
+    error = error / last%n
+    error = sqrt(error)
+
+    do, i=0, last%n
+        call calcOutputGradients(last%neurons(i), targets(i))
+    end do
+
+   first: do, ln = this%lnum-1, 1
+        second: do,i =0, this%layers(ln)%n
+         call calcHiddenGradients(this%layers(ln)%neurons(i), this%layers(ln+1))
+        end do second
+   end do first
+
+    first1: do, ln = this%lnum , 1
+        second1: do, i=0,this%layers(ln)%n
+           call adaptWeights(this%layers(ln)%neurons(i), this%layers(ln-1))
+        end do second1
+    end do first1     
+
+end subroutine backProp
+
+subroutine feedForwardNet(this,input,n)
+    use types
+    integer :: n
+    type(Net) :: this
+    real, dimension(0: n-1) :: input
+    integer :: i,ln 
+
+         do,i =0, this%layers(0)%n
+            this%layers(0)%neurons(i)%output = input(i)
+        end do
+
+   first: do, ln = 1, this%lnum
+        second: do,i =0, this%layers(ln)%n
+            call feedForward(this%layers(ln)%neurons(i), this%layers(ln-1))
+        end do second
+   end do first
+
+end subroutine feedForwardNet
+
