90fcb662d5033d610e93232a928bc6b4ca161dfb
m87 <m87blackhole@gmail.com>
2017-02-18 10:13:44 +0100

    layer constructor

--- src/layer.f95
+++ src/layer.f95
@@ -1,63 +1,67 @@
 module nt_LayerModule
     use nt_TypesModule
     use nt_NeuronModule
+    use nt_InitMethodsModule
     implicit none
 
-    public :: nt_hiddenLayerInit, nt_firstLayerInit, nt_lastLayerInit
+    public :: nt_layerInit
+
+    interface nt_layerInit
+        module procedure nt_layerInit_random, nt_layerInit_custom
+    end interface
 
     contains
-
-        subroutine nt_hiddenLayerInit(this, layerSize, nextLayerSize, id, bias)
+        subroutine nt_layerInit_custom(this, layerType, layerSize, nextLayerSize, id, weightInitMethod, weightInitMethodArgs)
             type(nt_Layer) :: this
             integer :: layerSize
             integer :: nextLayerSize
             integer :: id
             logical :: bias
             integer :: i
+            integer :: layerType
+            real :: weightInitMethodArgs(0:)
+            interface
+                subroutine weightInitMethod(weight, args)
+                    real, intent(out) :: weight
+                    real, intent(in) :: args(0:)
+                end subroutine weightInitMethod
+            end interface
 
             this%layerSize = layerSize
             this%id = id
-
+            
             allocate(this%neurons(0:layerSize - 1))
-            init_loop: do, i=0, layerSize
-               call nt_hiddenNeuronInit(this%neurons(i), nextLayerSize, bias)
-            end do init_loop
+            
+            if (layerType .eq. 0) then
+                do, i=0, layerSize
+                   call nt_inputNeuronInit(this%neurons(i), nextLayerSize, weightInitMethod, weightInitMethodArgs)
+                end do
+            !else if (layerType .eq. 2) then
+            !    output_loop: do, i=0, layerSize
+            !        call nt_outputNeuronInit(this%neurons(i))
+            !    end do output_loop
+            !else
+            !    hidden_loop: do, i=0, layerSize
+            !        call nt_hiddenNeuronInit(this%neurons(i), nextLayerSize, weightInitMethod, weightInitMethodArgs)
+            !    end do hidden_loop
+            end if
 
-        end subroutine nt_hiddenLayerInit
 
-        subroutine nt_firstLayerInit(this, layerSize, nextLayerSize, bias)
+        end subroutine nt_layerInit_custom
+
+        subroutine nt_layerInit_random(this, layerType, layerSize, nextLayerSize, id)
             type(nt_Layer) :: this
             integer :: layerSize
             integer :: nextLayerSize
-            integer :: i
+            integer :: id
             logical :: bias
-
-            this%layerSize = layerSize
-            this%id = 0
-
-            allocate(this%neurons(0:layerSize - 1))
-            init_loop: do, i=0, layerSize
-               call nt_inputNeuronInitneuronInit(this%neurons(i), nextLayerSize, bias)
-            end do init_loop
-
-        end subroutine nt_firstLayerInit
-
-        subroutine nt_lastLayerInit(this, layerSize, bias, lastId)
-            type(nt_Layer) :: this
-            integer :: layerSize
-            integer :: lastId
             integer :: i
-            logical :: bias
+            integer :: layerType
 
-            this%layerSize = layerSize
-            this%id = lastId
+            call nt_layerInit_custom(this, layerType, layerSize, nextLayerSize, id, randomInit, (/ 0.0 /))
 
-            allocate(this%neurons(0:layerSize - 1))
-            init_loop: do, i=0, layerSize
-               call nt_outputNeuronInit(this%neurons(i), 0, bias)
-            end do init_loop
+        end subroutine nt_layerInit_random
 
-        end subroutine nt_lastLayerInit
 
-        b
+
 end module nt_LayerModule
--- test/src/layerTest.f95
+++ test/src/layerTest.f95
@@ -1,6 +1,6 @@
 program layerTest
     use nt_TypesModule
-    use nt_NeuronModule
+    use nt_LayerModule
     use sft_AssertModule
     use sft_SuiteModule
     implicit none
@@ -8,7 +8,41 @@ program layerTest
     type(sft_Suite) :: testSuite
 
     call sft_init(testSuite)
+
+    call sft_run(testSuite, shouldCorrectlyInitInputLayer)
+    !call sft_run(testSuite, shouldCorrectlyInitOutputLayer)
+    !call sft_run(testSuite, shouldCorrectlyInitHiddenLayer)
     
     call sft_summary(testSuite)
+    
+    contains 
+        function shouldCorrectlyInitInputLayer() result(res)
+            logical :: res
+            !given
+            type(nt_Layer) :: layer
+            
+            !when
+            call nt_layerInit(layer, 0, 5, 10, 0)
+
+            !then
+            res = sft_assertEqual(layer%layerSize, 5) &
+                .and. sft_assertEqual(layer%id, 0) &
+                .and. sft_assertEqual(layer%neurons(0)%nextLayerSize, 10)
+
+        end function shouldCorrectlyInitInputLayer
+
+        function shouldCorrectlyInitOutputLayer() result(res)
+            logical :: res
+            type(nt_Layer) :: layer
+
+            res = .TRUE.
+        end function shouldCorrectlyInitOutputLayer
+
+        function shouldCorrectlyInitHiddenLayer() result(res)
+            logical :: res
+            type(nt_Layer) :: layer
+            
+            res = .TRUE.
+        end function shouldCorrectlyInitHiddenLayer
 
 end program layerTest
